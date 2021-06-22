ba3e8575249fa7a5ce2ebffacb6d46118b46a984
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-22 14:31:49 +0200

    Add files via upload
    Met Terminal gerunt. 
    Commenting out toegepast.

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy Studenten")
+console.log("Wieneke")
+/* Commenting out my math
+let multiplication = 123094 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division", division);
+*/
